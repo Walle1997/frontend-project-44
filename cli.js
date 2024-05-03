f930db840ae30fb7ce2dc215991bff3ecfe3efb0
Farid Aliev <farid-keltuzed@mail.ru>
2024-05-03 19:19:45 +0300

    .

--- cli.js
+++ cli.js
@@ -1,1 +1,8 @@
-answer
+import readlineSync from 'readline-sync';
+ const welcomeMessage = () => {
+    console.log('Welcome to the Brain Games!');
+    const userName = readlineSync.question('May i have your name? ');
+    console.log(`Hello, ${userName}!`);
+ };
+
+ export default welcomeMessage;
