f47db2179564a9e9cbed4bd6f6fbcb8909103880
Farid Aliev <farid-keltuzed@mail.ru>
2024-05-13 13:45:54 +0300

    .

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,8 @@
 import { getQuestion } from './helpers.js';
 
-const UserHello = () => {
+const getUserHello = () => {
   console.log('Welcome to the Brain Games!');
   const name = getQuestion('May I have your name? ');
   return `Hello, ${name}!`;
 };
-export default UserHello;
+export default getUserHello;
