fff81547e8f8b71d058da43a42e4c7bf875118e5
Farid Aliev <farid-keltuzed@mail.ru>
2024-05-15 15:46:37 +0300

    .

--- src/helpers.js
+++ src/helpers.js
@@ -2,7 +2,7 @@ import readlineSync from 'readline-sync';
 
 const getRandom = (min = 1, max = 100) => Math.floor(Math.random() * (max - min + 1)) + min;
 const getQuestion = (description) => readlineSync.question(description);
-readlineSync.question();
+
 
 const getNod = (a, b) => {
   let t = a;
