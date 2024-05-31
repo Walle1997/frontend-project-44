215697346008f930e651a94ab4b093428df9ebe3
Farid Aliev <farid-keltuzed@mail.ru>
2024-05-31 23:46:27 +0300

    .

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,20 @@
-import globals from "globals";
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable max-len */
+/* eslint-disable no-underscore-dangle */
+import globals from 'globals';
 
-import path from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-import pluginJs from "@eslint/js";
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import pluginJs from '@eslint/js';
 
 // mimic CommonJS variables -- not needed if using CommonJS
+// eslint-disable-next-line no-underscore-dangle
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({baseDirectory: __dirname, recommendedConfig: pluginJs.configs.recommended});
+const compat = new FlatCompat({ baseDirectory: __dirname, recommendedConfig: pluginJs.configs.recommended });
 
 export default [
-  {languageOptions: { globals: globals.browser }},
-  ...compat.extends("airbnb"),
+  { languageOptions: { globals: globals.browser } },
+  ...compat.extends('airbnb'),
 ];
